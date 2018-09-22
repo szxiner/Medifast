c60d1aeab65bfc128c6d9f0b638794a12ffcd8f6
Travis <trabrook@umail.iu.edu>
2018-09-22 16:06:56 -0400

    Heroku Compatible?

--- Medifast/Medifast/settings.py
+++ Medifast/Medifast/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#import django_heroku
+#django_heroku.settings(locals())
