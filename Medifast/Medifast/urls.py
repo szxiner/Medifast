9c98105cd4ab4ca73fd5f558182e145343ff210e
Travis <trabrook@umail.iu.edu>
2018-09-30 18:28:27 -0400

    Minor changes

--- Medifast/Medifast/urls.py
+++ Medifast/Medifast/urls.py
@@ -20,10 +20,9 @@ from users import views
 from pages.views import FrontendRenderView
 
 urlpatterns = [
-
     path('api-auth', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('api-users', include('users.urls')),
     #path('(?P<path>.*)', FrontendRenderView, name=home),
 ]
 
