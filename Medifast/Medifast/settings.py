f924b4a082a6f46b471790b182d15e78c16f1a4b
Xiner Zhang <szxiner@XinerdeMacBook-Pro.local>
2018-10-01 04:16:59 -0400

    password encryption done. yeah

--- Medifast/Medifast/settings.py
+++ Medifast/Medifast/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
