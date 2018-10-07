abad0e14f107cbb4c015241c868ea7ba2d25ac8b
Xiner Zhang <szxiner@149-161-155-86.dhcp-bl.indiana.edu>
2018-10-07 15:37:24 -0400

    just start

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 export default class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isAuth: false
+    };
+  }
+
+  // componentDidMount() {
+  //   if (this.props.auth.isAuth) {
+  //     <Redirect to="/dashboard" />;
+  //   }
+  // }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuth) {
+      <Redirect to="/dashboard" />;
+    }
+  }
   render() {
     return <div>Hello Dashboard</div>;
   }
