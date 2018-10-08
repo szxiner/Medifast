12e2c1ac4f9f1e4fa43d7042d74dd8d7c7662275
Manjeet Pandey <mankumarasdf@gmail.com>
2018-10-07 21:39:22 -0400

    Patient History

--- Medifast/Patient_profile/models.py
+++ Medifast/Patient_profile/models.py
@@ -6,8 +6,20 @@ class Patient_profile(models.Model):
     email = models.EmailField(null=True)
     First_name = models.CharField(max_length=100, null=True)
     Last_Name = models.CharField(max_length=100, null=True)
+    gender = models.CharField(max_length=10, null=True)
     DOB = models.DateField(null=True)
 
     def __str__(self):
-        return self.email
+        return self.username
+
+class Patient_history(models.Model):
+    username = models.CharField(max_length=100)
+    issue = models.CharField(max_length=200, null=True)
+    doctor = models.CharField(max_length=100, null=True)
+    expenditure = models.IntegerField(null=True)
+    date = models.DateField(null=True)
+
+    def __str__(self):
+        return self.username
+
 
