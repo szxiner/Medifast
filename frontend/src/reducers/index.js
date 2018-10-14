c62aacf64faa1642b2a8c2f17c09969596aaa776
Xiner Zhang <szxiner@XinerdeMacBook-Pro.local>
2018-10-13 20:21:53 -0400

    user list view for insurance provide

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,4 +2,15 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import profileReducer from "./profileReducer";
 
-export default combineReducers({ auth: authReducer, profile: profileReducer });
+const initialUser = { userType: "Insurance" };
+
+// this is the ES2015 syntax for setting a default value for state in the function parameters
+function userReducer(state = initialUser, action) {
+  return state;
+}
+
+export default combineReducers({
+  auth: authReducer,
+  profile: profileReducer,
+  user: userReducer
+});
