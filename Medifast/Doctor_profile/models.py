75e17386f13ab7cdb0e6c97e6b6c87d97d29a033
Manjeet Pandey <mankumarasdf@gmail.com>
2018-10-14 22:30:25 -0400

    Doctors appointment and review

--- Medifast/Doctor_profile/models.py
+++ Medifast/Doctor_profile/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import *
 
 class Doctor_profile(models.Model):
     username = models.CharField(max_length=100, primary_key=True) 
@@ -11,7 +12,15 @@ class Doctor_profile(models.Model):
     Hospital = models.CharField(max_length=100, null=True)
     rating = models.IntegerField( null=True)
     hourly_charge = models.IntegerField( null=True)
-
+    location = models.CharField(max_length=100, null= True)
 
     def __str__(self):
         return self.username
+
+class Doctor_appointment(models.Model):
+    username = models.CharField(max_length=100, null=True)
+    appointment = models.DateTimeField( null=True)
+
+class Doctor_reviews(models.Model):
+    username = models.CharField(max_length=100, null=True)
+    reviews = models.CharField(max_length=1000, null=True)
