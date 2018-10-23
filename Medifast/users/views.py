1d6e0659f3c6dba9efb5bd99937e662108ae627c
Travis <trabrook@umail.iu.edu>
2018-10-23 19:29:44 -0400

    OauthInitialBackend

--- Medifast/users/views.py
+++ Medifast/users/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Account
 from .serializers import AccountSerializer
+from .serializers import SocialSerializer
 from django.core import serializers
 from rest_framework import viewsets
 from rest_framework.views import APIView
@@ -20,13 +21,79 @@ from django.shortcuts import render, redirect
 from .forms import TokenVerificationForm
 from .decorators import twofa_required
 from rest_framework.test import APIRequestFactory
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from social_django.utils import psa
+from requests.exceptions import HTTPError
+
+
+
 
 # Lists all accounts
 # /users
 authy_api = AuthyApiClient(settings.ACCOUNT_SECURITY_API_KEY)
 authy_id = None
 username = None
+
+# OAuth2 API
+@api_view(http_method_names=['POST'])
+@permission_classes([AllowAny])
+@psa()
+def exchange_token(request, backend):
+    """
+    Exchange an OAuth2 access token for one for this site
+    ## Request format
+    Requests must include the following field
+    - `access_token`: The OAuth2 access token provided by the auth provider
+    """
+    serializer = SocialSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+
+        try:
+            nfe = settings.NON_FIELD_ERRORS_KEY
+        except AttributeError:
+            nfe = 'non_field_errors'
+
+        try:
+            # this line, plus the psa decorator above, are all that's necessary to
+            # get and populate a user object for any properly enabled/configured backend
+            # which python-social-auth can handle.
+            user = request.backend.do_auth(serializer.validated_data['access_token'])
+        except HTTPError as e:
+            # An HTTPError bubbled up from the request to the social auth provider.
+            # This happens, at least in Google's case, every time you send a malformed
+            # or incorrect access key.
+            return Response(
+                {'errors': {
+                    'token': 'Invalid token',
+                    'detail': str(e),
+                }},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user:
+            if user.is_active:
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key})
+            else:
+                # user is not active; at some point they deleted their account,
+                # or were banned by a superuser. They can't just log in with their
+                # normal credentials anymore, so they can't log in with social
+                # credentials either.
+                return Response(
+                    {'errors': {nfe: 'This user account is inactive'}},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        else:
+            # Unfortunately, PSA swallows any information the backend provider
+            # generated as to why specifically the authentication failed;
+            # this makes it tough to debug except by examining the server logs.
+            return Response(
+                {'errors': {nfe: "Authentication Failed"}},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
 
 # API to authorize user when logging in
 class AuthAccount(APIView):
