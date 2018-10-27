9998d4f5a04051aab2065d1bed0fa161146da32d
Travis <trabrook@umail.iu.edu>
2018-10-27 15:24:25 -0400

    user patch

--- Medifast/users/views.py
+++ Medifast/users/views.py
@@ -53,47 +53,12 @@ class AuthAccount(APIView):
                 return True
             return False
 
-'''
-class AccountUpdate(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Account.objects.get(pk=pk)
-        except Account.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = AccountSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = AccountSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-'''
-
 # API to get, update, delete specific user (Update using post)
-class AccountDetail(generics.RetrieveDestroyAPIView):
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     lookup_field = 'username'
 
-
-
-    def post(self, request, format=None):
-
-        users = Account.objects.filter(username=request.data['username'])
-        if len(users) != 0:
-            user = users.first()
-            serializer = AccountSerializer(user, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(True, status=status.HTTP_200_OK)
-        return Response(False, status=status.HTTP_400_BAD_REQUEST)
 '''
     def put(self, request, format=None):
         users = Account.objects.filter(username=request.data['username'])
@@ -105,6 +70,8 @@ class AccountDetail(generics.RetrieveDestroyAPIView):
                 return Response(True, status=status.HTTP_200_OK)
         return Response(False, status=status.HTTP_400_BAD_REQUEST)
 '''
+#class AccountUpdate():
+
 
 # API to register new user to the database
 class AccountList(APIView):
