dd7eda4c7f729cc3757240cf4450f0ce011ef54d
bindumareedu <sriii.mareedu@gmail.com>
2018-10-28 18:57:25 -0400

    showalldoctors

--- frontend/src/components/Dashboard/DashboardSideBar.jsx
+++ frontend/src/components/Dashboard/DashboardSideBar.jsx
@@ -17,6 +17,7 @@ import { StyleSheet, css } from "aphrodite";
 import Column from "antd/lib/table/Column";
 import { connect } from "react-redux";
 import Map from "../../common/Map";
+import SearchDoctors from "../UserList/SearchDoctors";
 
 const Styles = StyleSheet.create({
   clickMe: {
@@ -28,11 +29,10 @@ const Styles = StyleSheet.create({
     width: "80%",
     height: "auto",
     padding: 50,
-    backgroundColor: "#CFD8DC",
+    backgroundColor: "#FAFAFA",
     fontFamily: " Pacifico",
     fontSize: 18
-  },
-  p: {}
+  }
 });
 
 class DashboardSideBar extends Component {
@@ -119,17 +119,19 @@ class DashboardSideBar extends Component {
           </Tab>
         </TabList>
         <TabPanel tabId="vertical-tab-one" component={Profile} />
-        <TabPanel tabId="vertical-tab-two" component={UserView} />
+        <TabPanel tabId="vertical-tab-two" component={SearchDoctors} />
         <TabPanel tabId="vertical-tab-three">
           <Jumbotron className={css(Styles.jumbotron)}>
             <h1>Medicare Gold Plus®</h1>
-            <p font-family="Pacifico">
+            <p1>
               Medicare Gold Plus is a Medicare Advantage health maintenance
               organization (HMO) plan that includes all the benefits of Original
               Medicare and may include prescription drug coverage and many
               extras.
-            </p>
-            <p>
+            </p1>
+            <p1>
+              <br />
+              <br />
               <Button
                 bsStyle="primary"
                 bsSize="large"
@@ -138,22 +140,23 @@ class DashboardSideBar extends Component {
               >
                 Plan Details
               </Button>
-
+              <br />
+              <br />
               <Modal show={this.state.show} onHide={this.handleClose}>
                 <Modal.Header closeButton>
                   <Modal.Title>Benefits</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                  <p>
+                  <p1>
                     With an HMO plan, out-of-pocket costs are generally lower,
                     and they may be more predictable than with other types of
                     plans. So you’ll have a better idea of how much you’ll spend
                     on healthcare during the year.
-                  </p>
-                  <p>
+                  </p1>
+                  <p1>
                     Medicare Gold Plus HMO plans cover all the benefits of
                     Original Medicare and much more, including:
-                  </p>
+                  </p1>
                   <ul />
                   <li>
                     Choice of a primary care physician in the plan network
@@ -176,10 +179,11 @@ class DashboardSideBar extends Component {
                   <Button onClick={this.handleClose}>Close</Button>
                 </Modal.Footer>
               </Modal>
-            </p>
+            </p1>
 
             <div className={css(Styles.map)}>
-              <p>Our Location:</p>
+              <p1>Our Location:</p1>
+              <br />
               <Map lat={40.0287983} lng={-105.2524828} />
             </div>
           </Jumbotron>
