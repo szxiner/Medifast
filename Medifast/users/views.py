69dfef0e7b82adce1bfcdb96ca6b2c5fe93eeda7
Travis <trabrook@umail.iu.edu>
2018-10-31 07:50:29 -0400

    Fix pwd reset

--- Medifast/users/views.py
+++ Medifast/users/views.py
@@ -115,7 +115,8 @@ class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
             print(serializer)
             if serializer.is_valid():
                 print("valid")
-                if(serializer.validated_data['phone_number'] is not None):
+                try:
+                    serializer.validated_data['phone_number']
                     print("Gooogle")
                     authy_user = authy_api.users.create(
                     serializer.validated_data['email'],
@@ -138,7 +139,7 @@ class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
                         the_Username = serializer.validated_data['username']
                         authy_id = authy_user.id
                         return Response(True, status=status.HTTP_200_OK)
-                else:
+                except:
                     print("Not goog")
                     serializer.save()
                     return Response(True, status=status.HTTP_200_OK)
