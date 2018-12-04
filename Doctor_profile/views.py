9be006243c552f034309a81e5f719ed60794b712
Manjeet Pandey <mankumarasdf@gmail.com>
2018-12-03 22:09:18 -0500

    email should work now

--- Doctor_profile/views.py
+++ Doctor_profile/views.py
@@ -100,12 +100,16 @@ class Doctor_bookings_view(APIView):
             time = serializer.data['btime']
             date = serializer.data['bdate']
             message = 'Congratulations! Your appointment is confirmed for ' + str(date) + ' at ' + str(time[0])
-            profile = Doctor_profile.objects.filter(username=request.data['docusername'])
-            D_serial = Doctor_profile_serializer(profile, many=True)
+            profile = Account.objects.filter(username=request.data['docusername'])
+            D_serial = AccountSerializerr(profile, many=True)
             patient = Account.objects.filter(username=request.data['patientusername'])
             P_serial = AccountSerializer(patient, many=True)
-            # email_id = [D_serial.data[0]['email'],P_serial.data[0]['email']]
-            # self.send_email(message,email_id)
+            email_id = [D_serial.data[0]['email'],P_serial.data[0]['email']]
+            self.send_email(message,email_id)
+            history = {'username':request.data['patientusername'],'issue':request.data['issue'],'date':request.data['bdate'],'expenditure':D_serial.data[0]['hourly_charge'],'doctor':request.data['docusername']}
+            patient_history_serializer = Patient_history_serializer(data=history)
+            if patient_history_serializer.is_valid():
+                patient_history_serializer.save()
             return Response(True, status=status.HTTP_201_CREATED)
         else:
             return Response(False, status=status.HTTP_400_BAD_REQUEST)
